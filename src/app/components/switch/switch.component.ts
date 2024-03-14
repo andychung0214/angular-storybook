b1694ec7db834907be27a9107e5ba228a0919933
鍾岳儒 <andy6756@eri.com.tw>
2024-03-14 16:53:13 +0800

    add Switch component and story

---
+++ src/app/components/switch/switch.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './switch.component.html',
+  styleUrls: ['./switch.component.scss']
+})
+export class SwitchComponent {
+
+}
---
+++ src/stories/switch.stories.ts
@@ -0,0 +1,37 @@
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+import { importProvidersFrom } from '@angular/core';
+import { SwitchComponent } from 'src/app/components/switch/switch.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const meta: Meta<SwitchComponent> = {
+  title: '數據輸入元件/Checkbox',
+  component: SwitchComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+  args: { onClick: fn() },
+  decorators: [
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+        // 添加其他全局模块或服务
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<SwitchComponent>;
+
+export const Default: Story = {
+  args: {
+    checked: false,
+    label: 'Checkbox',
+  },
+};
+
+
