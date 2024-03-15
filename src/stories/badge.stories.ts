b5e6f80d0b057683ee272ceccec0daece276ad8a
Andy Chung <andy6756@gmail.com>
2024-03-16 00:47:42 +0800

    add badge component and story

---
+++ src/stories/badge.stories.ts
@@ -0,0 +1,36 @@
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { BadgeComponent } from 'src/app/components/badge/badge.component';
+
+const meta: Meta<BadgeComponent> = {
+  title: 'Data-Input-Components/Badge',
+  component: BadgeComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+  args: { onChange: fn() },
+  decorators: [
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<BadgeComponent>;
+
+export const Default: Story = {
+  args: {
+    isChecked: false,
+  },
+};
+
+
