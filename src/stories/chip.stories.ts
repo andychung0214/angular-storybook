3c2374eb12e1509bebc956e2b70c43af7863fea1
Andy Chung <andy6756@gmail.com>
2024-03-16 00:50:01 +0800

    add chip component and story

---
+++ src/stories/chip.stories.ts
@@ -0,0 +1,36 @@
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ChipComponent } from 'src/app/components/chip/chip.component';
+
+const meta: Meta<ChipComponent> = {
+  title: 'Data-Input-Components/Chip',
+  component: ChipComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+  args: { onChange: fn() },
+  decorators: [
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<ChipComponent>;
+
+export const Default: Story = {
+  args: {
+    isChecked: false,
+  },
+};
+
+
