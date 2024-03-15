2cc171782787133b993df4e0bf90a6508a0ae668
Andy Chung <andy6756@gmail.com>
2024-03-16 00:49:03 +0800

    add card component and story

---
+++ src/stories/card.stories.ts
@@ -0,0 +1,36 @@
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CardComponent } from 'src/app/components/card/card.component';
+
+const meta: Meta<CardComponent> = {
+  title: 'Data-Input-Components/Card',
+  component: CardComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+  args: { onChange: fn() },
+  decorators: [
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<CardComponent>;
+
+export const Default: Story = {
+  args: {
+    isChecked: false,
+  },
+};
+
+
