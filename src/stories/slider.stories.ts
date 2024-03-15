0a7a77a7327feab4e22aeac6d848bc36fa7d0ce3
Andy Chung <andy6756@gmail.com>
2024-03-16 01:05:34 +0800

    add slider component and story

---
+++ src/stories/slider.stories.ts
@@ -0,0 +1,36 @@
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SliderComponent } from 'src/app/components/slider/slider.component';
+
+const meta: Meta<SliderComponent> = {
+  title: 'Data-Input-Components/Slider',
+  component: SliderComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+  args: { onChange: fn() },
+  decorators: [
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<SliderComponent>;
+
+export const Default: Story = {
+  args: {
+    isChecked: false,
+  },
+};
+
+
