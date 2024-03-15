40902d5dc3c7a1fa5565a513084094c1bba647a5
Andy Chung <andy6756@gmail.com>
2024-03-16 01:20:14 +0800

    update rate story

---
+++ src/stories/rate.stories.ts
@@ -0,0 +1,36 @@
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RateComponent } from 'src/app/components/rate/rate.component';
+
+const meta: Meta<RateComponent> = {
+  title: 'Data-Input-Components/ProgressCircle',
+  component: RateComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+  args: { onChange: fn() },
+  decorators: [
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<RateComponent>;
+
+export const Default: Story = {
+  args: {
+    isChecked: false,
+  },
+};
+
+
