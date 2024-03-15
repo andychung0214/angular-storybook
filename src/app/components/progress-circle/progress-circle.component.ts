23d758c3777b0c1d7046eb6de55ddb20da37d2f8
Andy Chung <andy6756@gmail.com>
2024-03-16 01:02:40 +0800

    add progress circle component and story

---
+++ src/app/components/progress-circle/progress-circle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-circle',
+  templateUrl: './progress-circle.component.html',
+  styleUrls: ['./progress-circle.component.scss']
+})
+export class ProgressCircleComponent {
+
+}
---
+++ src/stories/progressCircle.stories.ts
@@ -0,0 +1,36 @@
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ProgressCircleComponent } from 'src/app/components/progress-circle/progress-circle.component';
+
+const meta: Meta<ProgressCircleComponent> = {
+  title: 'Nav-Components/ProgressCircle',
+  component: ProgressCircleComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+  args: { onChange: fn() },
+  decorators: [
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<ProgressCircleComponent>;
+
+export const Default: Story = {
+  args: {
+    isChecked: false,
+  },
+};
+
+
