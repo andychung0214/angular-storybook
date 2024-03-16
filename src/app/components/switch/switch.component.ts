d7d1620fdb0895b3b39c7562960eb439b59d2a95
Andy Chung <andy6756@gmail.com>
2024-03-17 00:01:34 +0800

    update switch component and story

--- src/app/components/switch/switch.component.ts
+++ src/app/components/switch/switch.component.ts
@@ -1,21 +1,41 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ElementRef, AfterViewInit } from '@angular/core';
+import { ColorService } from 'src/app/services/color.service';
 
 @Component({
   selector: 'app-switch',
   templateUrl: './switch.component.html',
   styleUrls: ['./switch.component.scss']
 })
-export class SwitchComponent {
-  @Input() backgroundColor?: string;
+export class SwitchComponent implements AfterViewInit {
   @Input() isChecked: boolean = false;
   @Input() isDisabled: boolean = false;
+  @Input() size: 'default' | 'small' = 'default';
   @Input() themeColor: string = 'primary';
-  @Output() onChange: EventEmitter<boolean> = new EventEmitter();
+  @Input() checkedChildren: string = '';
+  @Input() unCheckedChildren: string = '';
 
-  toggleSwitch(): void {
+  @Output() onChange = new EventEmitter<boolean>();
+
+  labelWidth: number = 0;
+  thumbSize: number = 18;
+  switchWidth: number = this.thumbSize * 2; // Initial value, adjust as needed
+  switchColor: string = '';
+
+  constructor(private colorService: ColorService, private elRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.adjustLabelWidth();
+  }
+
+  adjustLabelWidth() {
+    // Adjust your label width calculation based on your actual element refs
+     /* your logic to calculate switch width based on label width */;
+    this.switchWidth = this.labelWidth + this.thumbSize * 2;
+  }
+
+  handleClick() {
     if (!this.isDisabled) {
-      this.isChecked = !this.isChecked;
-      this.onChange.emit(this.isChecked);
+      this.onChange.emit(!this.isChecked);
     }
   }
 }
--- src/stories/switch.stories.ts
+++ src/stories/switch.stories.ts
@@ -1,15 +1,15 @@
 import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
 import { fn } from '@storybook/test';
 import { importProvidersFrom } from '@angular/core';
-import { SwitchComponent } from 'src/app/components/switch/switch.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SwitchComponent } from 'src/app/components/switch/switch.component';
 
 const meta: Meta<SwitchComponent> = {
   title: 'Data-Input-Components/Switch',
   component: SwitchComponent,
   tags: ['autodocs'],
   argTypes: {
-    backgroundColor: {
+    themeColor: {
       control: 'color',
     },
   },
@@ -29,8 +29,21 @@ type Story = StoryObj<SwitchComponent>;
 
 export const Default: Story = {
   args: {
-    isChecked: false,
+
+  isChecked: false,
+  isDisabled: false,
+  size: 'default',
+  themeColor: 'primary',
+  checkedChildren: 'On',
+  unCheckedChildren: 'Off',
   },
 };
 
+export const Checked: Story = {
+  args: {
+
+    ...Default.args,
+    isChecked: true,
+  },
+};
 
