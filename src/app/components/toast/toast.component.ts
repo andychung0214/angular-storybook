2b72cc6fe86c23c719566df1f4cd3ebd0c7afb25
Andy Chung <andy6756@gmail.com>
2024-03-18 11:43:57 +0800

    update toast component and story

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, TemplateRef } from '@angular/core';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
-  styleUrls: ['./toast.component.scss']
+  styleUrls: ['./toast.component.scss'],
 })
-export class ToastComponent {
+export class ToastComponent implements OnInit, OnDestroy {
+  @Input() content: string | TemplateRef<any> | null = null;
+  @Input() type: 'success' | 'info' | 'warn' | 'error' = 'info';
+  @Input() duration: number = 3000;
 
+  constructor(private toastService: ToastService) {}
+
+  ngOnInit(): void {
+    setTimeout(() => this.toastService.removeToast(this), this.duration);
+  }
+
+  ngOnDestroy(): void {
+    console.log('Toast destroyed');
+  }
 }
--- src/stories/toast.stories.ts
+++ src/stories/toast.stories.ts
@@ -1,21 +1,22 @@
-import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { applicationConfig, moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { fn } from '@storybook/test';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ToastComponent } from 'src/app/components/toast/toast.component';
+import { ToastService } from 'src/app/services/toast.service';
 
 const meta: Meta<ToastComponent> = {
   title: 'Feedback-Components/Toast',
   component: ToastComponent,
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: {
-      control: 'color',
-    },
-  },
-  args: { onChange: fn() },
   decorators: [
+    // moduleMetadata({
+    //   // Import the ToastModule to make the ToastComponent available
+    //   imports: [Toasmo],
+    //   // Provide the ToastService
+    //   providers: [ToastService],
+    // }),
     applicationConfig({
       providers: [
         importProvidersFrom(BrowserAnimationsModule),
@@ -26,10 +27,29 @@ const meta: Meta<ToastComponent> = {
 
 export default meta;
 type Story = StoryObj<ToastComponent>;
+// const toastService: ToastService = argTypes.toastService;
+
+// export const Template: Story = {
+
+//   return {
+//     props: args,
+//     template: `
+//       <button (click)="showSuccess()">Show Success Toast</button>
+//       <button (click)="showError()">Show Error Toast</button>
+//     `,
+//     // Define methods to show different types of toasts
+//     showSuccess() {
+//       toastService.showToast('Success message', 'success');
+//     },
+//     showError() {
+//       toastService.showToast('Error message', 'error');
+//     },
+//   };
+// };
+
 
 export const Default: Story = {
   args: {
-    isChecked: false,
   },
 };
 
