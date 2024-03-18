125c1e2f831715e665e19293f6debdc88d74e0ce
Andy Chung <andy6756@gmail.com>
2024-03-19 00:26:24 +0800

    update modal component and story

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnChanges, SimpleChanges, HostListener, ElementRef } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, HostListener, ElementRef, ViewEncapsulation, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { animate, style, transition, trigger } from '@angular/animations';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
+  standalone: true, // Mark as standalone
   styleUrls: ['./modal.component.scss'],
   animations: [
     trigger('fade', [
@@ -15,7 +17,10 @@ import { animate, style, transition, trigger } from '@angular/animations';
         animate('{{duration}}ms ease-in-out', style({ opacity: 0 })),
       ]),
     ]),
-  ]
+  ],
+  imports: [ButtonComponent], // Import ButtonComponent here,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ModalComponent implements OnChanges {
   @Input() isOpen: boolean = false;
